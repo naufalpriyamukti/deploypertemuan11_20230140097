1115ea7358ea95048ec2ec88d10ead9755e56fbf
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-05-18 03:49:17 +0700

    Import library

--- src/main/java/com/example/pertemuan11/model/User.java
+++ src/main/java/com/example/pertemuan11/model/User.java
@@ -1,4 +1,16 @@
 package com.example.pertemuan11.model;
 
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 }
