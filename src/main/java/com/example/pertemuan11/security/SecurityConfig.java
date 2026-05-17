dcb4669388b5930192bee6e97ca659dd58973a35
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-05-18 04:03:54 +0700

    tambah bean BCryptPasswordEncoder

--- src/main/java/com/example/pertemuan11/security/SecurityConfig.java
+++ src/main/java/com/example/pertemuan11/security/SecurityConfig.java
@@ -1,11 +1,25 @@
 package com.example.pertemuan11.security;
 
+import com.example.pertemuan11.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+
+    }
 }
