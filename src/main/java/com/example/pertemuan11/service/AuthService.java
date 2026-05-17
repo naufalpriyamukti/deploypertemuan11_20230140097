a1511928521a94debf8fa2cb48df0ae591f6a7be
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-05-18 04:31:32 +0700

    tambah service untuk pendaftaran

--- src/main/java/com/example/pertemuan11/service/AuthService.java
+++ src/main/java/com/example/pertemuan11/service/AuthService.java
@@ -1,4 +1,44 @@
 package com.example.pertemuan11.service;
 
+import com.example.pertemuan11.model.Profile;
+import com.example.pertemuan11.model.dto.RegisterRequest;
+import com.example.pertemuan11.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public class AuthService (UserRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register (RegisterRequest request) {
+
+        User user = User.builder()
+                .id(UUID.randomUUID().toString())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        Profile profile = Profile.builder()
+                .id(UUID.randomUUID().toString())
+                .nama(request.getNama())
+                .alamat(request.getAlamat())
+                .user(user)
+                .build()
+
+        User.setProfile(profile);
+
+        userRepository.save(user);
+    }
 }
+
+
+
