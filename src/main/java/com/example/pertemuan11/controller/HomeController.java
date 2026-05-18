43672e0a75feb1fc12c9421ab9416ceaa1896f33
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-05-18 12:48:00 +0700

    tambah HomeController untuk homepage

--- src/main/java/com/example/pertemuan11/controller/HomeController.java
+++ src/main/java/com/example/pertemuan11/controller/HomeController.java
@@ -1,4 +1,22 @@
 package com.example.pertemuan11.controller;
 
+import ch.qos.logback.core.model.Model;
+import com.example.pertemuan11.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final AuthService authService;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        User user = authService.getLoggedInUser();
+        model.addAttribute("user", user);
+        model.addAttribute("profile", user.getProfile());
+        return "home";
+    }
 }
